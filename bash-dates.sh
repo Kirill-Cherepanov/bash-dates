5a8be3bbd7eb132ee7f565f90ccc82259f9d4ac4
Cherepanov Kirill <cherkirr@gmail.com>
2023-07-03 20:10:09 +0300

    feat: add date manipulation and formatting

--- bash-dates.sh
+++ bash-dates.sh
@@ -8,3 +8,16 @@ randomize_time() {
 
   return "$hour:$minute:$second" $hour $minute $second
 }
+
+# Initial date
+DATE='Tue Jul 4 19:41:32 2023'
+function next_date {
+  local day=$(echo $(date -d "$DATE +1 day" +%d) | sed 's/^0//')
+  local time=$((RANDOM % 8 + 16)):$((RANDOM % 60)):$((RANDOM % 60))
+  DATE=$(date -d "$DATE +1 day" '+%a %b '"$day $time"' %Y')
+
+  # One-liner for testing in the terminal
+  # DATE=$(date -d "$DATE  1DAY" '+%a %b '"$(echo $(date -d "$DATE +1 day" +%d) | sed 's/^0//') $(($RANDOM % 8 + 16)):$(($RANDOM % 60)):$(($RANDOM % 60))"' %Y')
+  
+  FULL_DATE="$DATE +0300"
+}
